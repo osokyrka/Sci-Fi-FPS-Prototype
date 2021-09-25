e7b33c445b5a258e6bf61b2c72889cf9b70c229f
dxfferent <o.sokyrka@outlook.com>
2021-09-25 19:30:59 +0100

    Add files via upload
    added weapon shooting, reload, muzzle flash and hit effect as well as sound of the gun firing

--- Player.cs
+++ Player.cs
@@ -6,18 +6,39 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 3.5f, _jumpHeight = 3f, _gravity = 9.81f;
+    private float _yVelocity;
     private CharacterController _controller;
+    private bool _cursorLocked;
+    [SerializeField]
+    private GameObject _muzzleFlash;
+    [SerializeField]
+    private GameObject _hitMarkerPrefab;
+    [SerializeField]
+    private AudioSource _weaponAudioSource;
+    [SerializeField]
+    private int _currentAmmo;
+    private int _maxAmmo = 50;
+    private bool _reload = false;
     // Start is called before the first frame update
     void Start()
     {
         _controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        _cursorLocked = true;
+        _muzzleFlash.gameObject.SetActive(false);
+        _currentAmmo = _maxAmmo;
     }
 
     // Update is called once per frame
     void Update()
     {
         CalculateMovement();
+        CursorBehaviour();
+        FireWeapon();
+        CanReload();
     }
+
+
 
     void CalculateMovement()
     {
@@ -29,12 +50,75 @@ public class Player : MonoBehaviour
 
         velocity = transform.transform.TransformDirection(velocity);
         
-
+        //doesn't work with navmesh?
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            velocity.y *= _jumpHeight;
+            _yVelocity = _jumpHeight;
         }
+
+        _yVelocity -= _gravity; 
+        velocity.y = _yVelocity;
+        // 
 
         _controller.Move(velocity * Time.deltaTime);
     }
+
+    void CursorBehaviour()
+    {
+        if(Input.GetKeyDown(KeyCode.I) && _cursorLocked == true)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            _cursorLocked = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.I) && _cursorLocked == false)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            _cursorLocked = true;
+        }
+    }
+
+    void FireWeapon()
+    {
+        if (Input.GetButton("Fire1") && _currentAmmo > 0 && _reload == false)
+        {
+                Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+                RaycastHit hitInfo;
+                _muzzleFlash.gameObject.SetActive(true);
+                _currentAmmo--;
+
+                if (_weaponAudioSource.isPlaying == false)
+                {
+                    _weaponAudioSource.Play();
+                }
+                if (Physics.Raycast(ray, out hitInfo, 30))
+                {
+                    Debug.Log("HIT: " + hitInfo.transform.name);
+                    GameObject hitMarker = Instantiate(_hitMarkerPrefab, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+                    Destroy(hitMarker, 0.2f);
+                    //weapon damage
+                } 
+        }
+        else
+        {
+            _muzzleFlash.gameObject.SetActive(false);
+            _weaponAudioSource.Stop();
+        }
+        
+    }
+
+    void CanReload()
+    {
+        if(Input.GetKeyDown(KeyCode.R) && _reload == false && _currentAmmo < _maxAmmo)
+        {
+            _reload = true;
+            StartCoroutine(Reload());
+        }
+    }
+    IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(1.5f);
+        _currentAmmo = _maxAmmo;
+        _reload = false;
+    }
+        
 }
